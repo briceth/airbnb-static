ee0cbad0b9cf03e92470e3d93365225c7be5c12e
Brice Tessier Huort <tessierhuort@gmail.com>
2016-11-20 17:48:46 +0100

    essaye un truc

---
+++ source/javascripts/main.js
@@ -0,0 +1,19 @@
+// $(document).ready(function(){
+//         var offset = $(".navbar-wagon").offset().top;
+//         $(document).scroll(function(){
+//             var scrollTop = $(document).scrollTop();
+//             if(scrollTop > offset){
+//                 $(".navbar-wagon").css("position", "fixed");
+//             }
+//             else {
+//                 $(".navbar-wagon").css("position", "static");
+//             }
+//         });
+//     });
+
+// $(document).ready(function {
+//   $('.btn-submited').click(function {
+//     $('p').removeClass('animated fadeInUp')
+//     $('p').addClass('animated fadeInDown');
+//   });
+// });
