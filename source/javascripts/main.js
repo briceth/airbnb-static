33498c6f2a397ad58640d2960d77f710a98aa24e
Brice Tessier Huort <tessierhuort@gmail.com>
2016-11-21 13:53:45 +0100

    add parallax effect and footer information

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -3,12 +3,18 @@ $(document).ready(function(){
   var controller = new ScrollMagic.Controller();
 
   // parallax scene
+  // var parallaxTl = new TimelineMax();
+  // parallaxTl
+  //   .from('.content-wrapper', 1, {autoAlpha: 0, ease:Power0.easeNone})
+  //   .from('.bcg', 1, {y: '-50%', ease:Power0.easeNone}, 0)
+  //   ;
+
   var slideParallaxScene = new ScrollMagic.Scene({
     triggerElement: '.bcg-parallax',
     triggerHook: 1,
-    duration: '200%'
+    duration: '100%'
   })
-  .setTween(TweenMax.from('.bcg', 1, {y: '-30%', ease:Power0.easeNone}))
+  .setTween(TweenMax.from('.bcg', 0.3, {y: '-50%', ease:Power0.easeNone}, 0.3))
   .addTo(controller);
 
   // build a scene
