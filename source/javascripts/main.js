adee1689246ded6a28e37c682b3274d480678065
Brice Tessier Huort <tessierhuort@gmail.com>
2016-11-21 12:58:12 +0100

    add greenscok api to scrollmagic parallax

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -2,18 +2,23 @@ $(document).ready(function(){
   // Init ScrollMagic
   var controller = new ScrollMagic.Controller();
 
+  // parallax scene
+  var slideParallaxScene = new ScrollMagic.Scene({
+    triggerElement: '.bcg-parallax',
+    triggerHook: 1,
+    duration: '200%'
+  })
+  .setTween(TweenMax.from('.bcg', 1, {y: '-30%', ease:Power0.easeNone}))
+  .addTo(controller);
+
   // build a scene
   var ourScene = new ScrollMagic.Scene({
     triggerElement: '.features',
+    duration: '100%'
   })
   .setClassToggle('.features', 'fade-in') // add class to feature
-  .addIndicators({
-    name: 'fade scene',
-    colorTrigger: 'black',
-    indent: 100,
-    colorStart: 'green',
-  })
   .addTo(controller);
+
 });
 
 //         var offset = $(".navbar-wagon").offset().top;
