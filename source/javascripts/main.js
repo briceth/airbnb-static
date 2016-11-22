679c969b2d58b896f8bbb4d727e5469702a72ad3
Brice Tessier Huort <tessierhuort@gmail.com>
2016-11-22 17:37:04 +0100

    add loop to features

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -2,26 +2,13 @@ $(document).ready(function(){
   // Init ScrollMagic
   var controller = new ScrollMagic.Controller();
 
-  // parallax scene
-  // var parallaxTl = new TimelineMax();
-  // parallaxTl
-  //   .from('.content-wrapper', 1, {autoAlpha: 0, ease:Power0.easeNone})
-  //   .from('.bcg', 1, {y: '-50%', ease:Power0.easeNone}, 0)
-  //   ;
-
-  var slideParallaxScene = new ScrollMagic.Scene({
-    triggerElement: '.bcg-parallax',
-    triggerHook: 1
-  })
-  .setTween(TweenMax.from('.bcg', 0.3, {x: '-50%', ease:Power0.easeNone}, 0.3))
-  .addTo(controller);
-
-  // build a scene
-  var ourScene = new ScrollMagic.Scene({
-    triggerElement: '.features'
-  })
-  .setClassToggle('.features', 'fade-in') // add class to feature
-  .addTo(controller);
-
-
+  // loop through each features
+  $('.features').each(function() {
+    // build a scene
+    var ourScene = new ScrollMagic.Scene({
+      triggerElement: this,
+    })
+    .setClassToggle(this, 'fade-in') // add class to feature
+    .addTo(controller);
+  });
 });
